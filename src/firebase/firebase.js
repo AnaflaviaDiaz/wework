5b14b04ea8caa139b0db95c0b230d78f17c93cab
Anaflavia <anaflaviadiazmartel5a@gmail.com>
2018-08-03 11:29:23 -0500

    configurando entorno de trabajo

---
+++ src/firebase/firebase.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: 'AIzaSyC4pkn1wm3UT7duRnzKXdv4C7Brux7_NDs',
+  authDomain: 'wework-731bf.firebaseapp.com',
+  databaseURL: 'https://wework-731bf.firebaseio.com',
+  projectId: 'wework-731bf',
+  storageBucket: 'wework-731bf.appspot.com',
+  messagingSenderId: '1065680436892'
+};
+
+firebase.initializeApp(config);
---
+++ test/headless.js
@@ -0,0 +1,5 @@
+global.window = global;
+global.assert = require("chai").assert;
+
+// require archivo que testearan
+require("./test.spec.js");
