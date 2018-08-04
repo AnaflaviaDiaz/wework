3e3bbf9cf205bbd23572f244484a6507e703b140
Anaflavia <anaflaviadiazmartel5a@gmail.com>
2018-08-03 22:37:40 -0500

    closes #1

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
+const headerP = document.getElementById("header-principal");
+const formRegisterV = document.getElementById("register-visitor");
 
+// components
+headerP.innerHTML = headerPrincipal;
+formRegisterV.innerHTML = formRegisterVisitor;
+
+// se genera despues de traer los componentes
+const btnLoginAdmin = document.getElementById("btn-loggin-admin");
+const btnRegisterVisitor = document.getElementById("btn-register-visitor");
+
+// botones
+btnLoginAdmin.addEventListener("click", () => console.log("login admin"))
+btnRegisterVisitor.addEventListener("click", () => console.log("hola"));
---
+++ src/views/header-principal.js
@@ -0,0 +1,10 @@
+headerPrincipal = `
+<nav class="navbar navbar-expand-lg navbar-light bg-app" id="big-nav">
+	<a class="navbar-brand lite">
+		<img src="img/icons/identificacion-de-visitante.png" width="30" height="30" class="d-inline-block align-top" alt=""> Visitor
+	</a>
+	<form class="form-inline">
+		<button type="button" class="btn btn-outline-lite my-2 my-sm-0" id="btn-loggin-admin" type="submit">Administrador</button>
+	</form>
+</nav>
+`;
