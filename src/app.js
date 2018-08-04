0d3c1cafe7175eb35863143bc4e97ff10c269c05
Anaflavia <anaflaviadiazmartel5a@gmail.com>
2018-08-04 11:20:29 -0500

    creando #4

--- src/app.js
+++ src/app.js
@@ -4,6 +4,12 @@ const btnToLoginAdmin = document.getElementById("btn-to-loggin-admin");
 const btnOkRegisterVisitor = document.getElementById("btn-ok-register-visitor");
 const btnToMain = document.getElementById("btn-to-main");
 const btnOkLoginAdmin = document.getElementById("btn-ok-login-admin");
+const txtNameVisitor = document.getElementById("txt-name-visitor");
+const txtDniVisitor = document.getElementById("txt-dni-visitor");
+const txtCelVisitor = document.getElementById("txt-cel-visitor");
+const wrongName = document.getElementById("wrong-name");
+const wrongDni= document.getElementById("wrong-dni");
+const wrongCel = document.getElementById("wrong-cel");
 
 const goToLoginAdmin = () => {
 	sectionRegisterVisitor.style.display = "none";
@@ -15,13 +21,31 @@ const goToMain = () => {
 	sectionRegisterVisitor.style.display = "block";
 }
 
+const showValidateRegisterVisitor = (validate) => {
+	if (validate.name) wrongName.hidden = true;
+  else wrongName.hidden = false;
+
+  if (validate.dni) wrongDni.hidden = true;
+	else wrongDni.hidden = false;
+
+	if (validate.cel) wrongCel.hidden = true;
+  else wrongCel.hidden = false;
+
+  if (validate.name && validate.dni && validate.cel) console.log("todo correcto");
+}
+
 // botones
 btnToLoginAdmin.addEventListener("click", () => {
 	goToLoginAdmin();
 });
-btnOkRegisterVisitor.addEventListener("click", () => console.log("aceptar registro"));
+btnOkRegisterVisitor.addEventListener("click", () => {
+	const validate = validateRegisterVisitor(txtNameVisitor.value, txtDniVisitor.value, txtCelVisitor.value);
+	showValidateRegisterVisitor(validate);
+});
 
 btnToMain.addEventListener("click", () => {
 	goToMain();
 });
-btnOkLoginAdmin.addEventListener("click", () => console.log("aceptar login admin"));
+btnOkLoginAdmin.addEventListener("click", () => {
+	
+});
---
+++ src/js/data.js
@@ -0,0 +1,13 @@
+window.validateRegisterVisitor = (name, dni, cel) => {
+  let visitor = {
+    name: false,
+    dni: false,
+    cel: false
+  }
+
+  visitor.name = name.length > 3;
+  visitor.dni = dni.length === 8;
+  visitor.cel = cel.length >= 9;
+
+  return visitor;
+}
