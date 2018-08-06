3e2df8740df97cb59421e08753890953bd6d78be
Anaflavia <anaflaviadiazmartel5a@gmail.com>
2018-08-06 08:29:51 -0500

    camara setgtings

--- src/js/take-photo-visitor.js
+++ src/js/take-photo-visitor.js
@@ -5,38 +5,33 @@ const btnTakePhoto = document.getElementById('take-photo');
 const btnDeletePhoto = document.getElementById('delete-photo');
 const errorMessage = document.getElementById('error-message');
 const hidden_canvas = document.getElementById('canvass');
-
 // Utilizamos la funcion getUserMedia para obtener la salida de la webcam
-navigator.getMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
-
+navigator.getMedia = ( navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
 const hideUI = () => {
   controls.style.display = 'flex';
   video.style.display = 'flex';
   errorMessage.style.display = 'none';
 }
-
 const showMessageError = (errormsg, error) => {
   error = error || '';
-  if (error) {
+  if(error){
     errorMessage.innerHTML = error;
   }
   errorMessage.innerText = errormsg;
   hideUI();
   errorMessage.style.display = 'flex';
 }
-
 const showVideo = () => {
   hideUI();
   video.style.display = 'flex';
   controls.style.display = 'flex';
 }
-
-const takeSnapshot = () => {
+const takeSnapshot = () => { 
   const context = hidden_canvas.getContext('2d');
   const width = video.videoWidth;
   const height = video.videoHeight;
-
-  if (width && height) {
+ 
+  if (width && height) { 
     hidden_canvas.width = width;
     hidden_canvas.height = height;
     context.drawImage(video, 0, 0, width, height);
@@ -44,26 +39,26 @@ const takeSnapshot = () => {
     return hidden_canvas.toDataURL('image/png');
   }
 }
-
 if (!navigator.getMedia) {
   showMessageError('Tu navegador no soporta la funcion getMedia.');
 } else {
   // Solicitamos la camara
-  navigator.getMedia({
-    video: true,
-  }, (stream) => {
-    // A nuestro componente video le establecemos el src 
-    video.src = window.URL.createObjectURL(stream);
-    video.play();
-    video.onplay = () => {
-      showVideo();
-    };
-  }, (err) => {
-    showMessageError('Ocurrio un error al obtener el stream de la webcam: ' + err.name, err);
-  });
+  navigator.getMedia(
+    {
+      video: true,
+    }, (stream) => {
+      // A nuestro componente video le establecemos el src 
+      video.src = window.URL.createObjectURL(stream);
+      video.play();
+      video.onplay = () => {
+        showVideo();
+      };
+    }, (err) => {
+      showMessageError('Ocurrio un error al obtener el stream de la webcam: ' + err.name, err);
+    }
+  );
 }
-
-btnTakePhoto.addEventListener('click', (event) => {
+btnTakePhoto.addEventListener('click', (event) => { 
   event.preventDefault();
   const snap = takeSnapshot();
   imgPhoto.setAttribute('src', snap);
@@ -73,7 +68,6 @@ btnTakePhoto.addEventListener('click', (event) => {
   // delete_photo_btn.classList.remove('disabled');
   video.pause();
 });
-
 btnDeletePhoto.addEventListener('click', (event) => {
   event.preventDefault();
   imgPhoto.setAttribute('src', '');
