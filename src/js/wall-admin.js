15da52cdb20b60ab3452a845fa9fea2bd643759b
Anaflavia <anaflaviadiazmartel5a@gmail.com>
2018-08-07 21:01:57 -0500

    add img table visitors

--- src/js/wall-admin.js
+++ src/js/wall-admin.js
@@ -13,6 +13,7 @@ const tableVisitor = (visitor) => {
 	time = new Date(visitor.date);
 	bodyTableVisitors.innerHTML += `
 	<tr>
+		<td><img src="${visitor.photo}" width="80px"></td>
 		<td>${visitor.name}</td>
 		<td>${time.toLocaleString()}</td>
 	</tr>
